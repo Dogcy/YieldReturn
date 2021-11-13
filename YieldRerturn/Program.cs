0754c7912f93ea9bde061ffb65a08d12850ea20c
DESKTOP-J2B2HPC\Marc <ccy810127@gmail.com>
2021-11-13 23:59:25 +0800

    test

--- YieldRerturn/Program.cs
+++ YieldRerturn/Program.cs
@@ -8,11 +8,11 @@ namespace YieldRerturn
         private static List<int> myList = new List<int>();
         static void FillValues()
         {
-            //myList.Add(1);
-            //myList.Add(2);
-            //myList.Add(3);
-            //myList.Add(4);
-            //myList.Add(5);
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
         }
         static void Main(string[] args)
         {
